628f0bdb069f88e4aa6db7011f2475bcb79ce85f
Sumer Patel <sumerpatel.dev@gmail.com>
2024-11-20 22:25:53 -0800

    Setup deps & shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <h1>Hello Vreme users!</h1>
+      <Button>Hello</Button>
     </>
   )
 }
